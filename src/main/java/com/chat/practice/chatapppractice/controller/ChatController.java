25a2e4e23725d3a8f7d1a876aa87ff737bc99d32
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-19 21:13:44 +0900

    GET /chat/getMessage 작성된 메세지가 저장된 List 리턴

--- src/main/java/com/chat/practice/chatapppractice/controller/ChatController.java
+++ src/main/java/com/chat/practice/chatapppractice/controller/ChatController.java
@@ -6,6 +6,7 @@ import com.chat.practice.chatapppractice.dto.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,8 +19,8 @@ import java.util.List;
 public class ChatController {
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    @AllArgsConstructor
-    @Getter
+    // record: constructor, getter 가 들어있는 java에서 기본으로 제공하는 type. java 17부터 사용 가능
+    // lombok이 있어서 잘 안쓰게됨.
     public record WriteMessageResponse(Long id){
     }
 
@@ -30,9 +31,18 @@ public class ChatController {
 
         chatMessages.add(message);
 
-        return new RsData(
+        return new RsData<>(
                 "S-1",
                 "메세지가 작성되었습니다.",
                 new WriteMessageResponse(message.getId()));
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> getMessage() {
+        return new RsData<>(
+                "S-1",
+                "성공",
+                chatMessages);
     }
 }
